7d2ff5b9c98966b9c98412dd84c0e92b084996d8
Ivan Makovetskyi <im061056@gmail.com>
2026-05-19 15:11:41 -0700

    feat(public): add reusable sliding gallery component

--- shared/components/public/index.ts
+++ shared/components/public/index.ts
@@ -20,5 +20,6 @@ export { default as ProcessTimeline } from "./ProcessTimeline";
 export { default as Reveal } from "./Reveal";
 export { default as SafeImage } from "./SafeImage";
 export { default as SectionHeading } from "./SectionHeading";
+export { default as SlidingGallery } from "./SlidingGallery";
 export { default as StaggerContainer } from "./StaggerContainer";
 export { default as StatCard } from "./StatCard";
